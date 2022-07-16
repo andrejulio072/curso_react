42029a15b418e96c191f62332308fe2bb648b697
Andre Garcia <andrejulio072@hotmail.com>
2022-07-16 18:56:10 +0100

    programming

---
+++ 13_CURSO_GRATUITO/todo/src/components/FirstComponent.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function FirstComponent() {
+  // Algum comentario
+  <div>
+    <div className="Component"></div>;
+    <button onClick={handleClick}>Evento de Click</button>
+    <hr />
+    <button onClick={()=> console.log('teste')}>Evento no Elemento</button>
+  </div>;
+}
+
+export default FirstComponent;
---
+++ 13_CURSO_GRATUITO/todo/src/components/Hooks.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { useState } from "react";
+
+const Hooks = () => {
+  let idade = 30;
+  const [novaidade, setNovaidade] = useState(40);
+  const changeAge = () => {
+    idade = 31;
+    console.log(idade);
+  };
+  return (
+    <div>
+      <p> Idade: {idade}</p>
+      <button onClick ={changeAge}> Mudar idade</button>
+
+    </div>
+  )
+};
